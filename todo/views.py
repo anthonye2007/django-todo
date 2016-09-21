c716cd6f7eb0c4ee37f89ff8ded23422fa3a86ac
Anthony Elliott <anthonye2007@gmail.com>
2016-09-21 04:16:00 +0000

    Display tasks on home page

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 def tasklist(request):
-   return HttpResponse("A list of tasks")
+    todos = Todo.objects.all()
+    output = ", ".join([t.todo_text for t in todos])
+    return HttpResponse(output)
