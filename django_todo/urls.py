361ee4b9c480f5f218870f50830fe891d949d34b
Anthony Elliott <anthonye2007@gmail.com>
2016-09-21 03:15:27 +0000

    Add default url

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('todo.urls')), 
     url(r'^tasks/', include('todo.urls')),
     url(r'^admin/', admin.site.urls),
 
