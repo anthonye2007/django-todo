0e683984a34c8d96ba5f6585f0caf520a2558bac
Anthony Elliott <anthonye2007@gmail.com>
2016-09-21 04:01:51 +0000

    Add Todo model

--- todo/models.py
+++ todo/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    todo_text = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.todo_text
