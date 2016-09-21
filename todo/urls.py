f4e7486c9efe7d64fd33eb7fdcc662d5d3fadd6f
Anthony Elliott <anthonye2007@gmail.com>
2016-09-21 04:26:22 +0000

    Allow user to create task with hardcoded text

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.tasklist, name='index'),
+    url(r'create/', views.createtask, name='create-task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -7,3 +7,9 @@ def tasklist(request):
     todos = Todo.objects.all()
     output = ", ".join([t.todo_text for t in todos])
     return HttpResponse(output)
+
+def createtask(request):
+    todo = Todo(todo_text="Get eggs")
+    todo.save()
+    output = "Created todo: %s" % todo.todo_text
+    return HttpResponse(output)
